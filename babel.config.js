9d757e9095d6475b838b1a7db7522a2d22187fe5
Matias Del Carlo <mati.dc89@gmail.com>
2020-06-22 14:44:18 +1200

    Add config files

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = api => {
+    const isTest = api.env('test');
+    if(isTest) {
+        return {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    node: 'current',
+                  },
+                },
+              ],
+            ],
+          };
+    }
+    return {};
+  };
