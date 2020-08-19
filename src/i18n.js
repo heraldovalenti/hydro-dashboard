383db254575b1ffe6fe4c68466f4de013598a689
Heraldo Valenti <heraldovalenti@gmail.com>
2020-08-19 00:06:56 -0300

    Removed language detection capability temporarily.
    An issue was presenting in some browsers given that language key 'es' was not valid
    becuase other keys like 'es_AR' or 'es_ES' was presented by the browser.

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import XHR from 'i18next-xhr-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+// import LanguageDetector from 'i18next-browser-languagedetector';
 import translationEng from './locales/en/translation.js';
 import translationEs from './locales/es/translation.js';
 
@@ -17,10 +17,10 @@ const resources = {
 i18n
   .use(XHR)
   .use(initReactI18next) // passes i18n down to react-i18next
-  .use(LanguageDetector)
+  // .use(LanguageDetector)
   .init({
     resources,
-    // lng: 'en',
+    lng: 'es',
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
