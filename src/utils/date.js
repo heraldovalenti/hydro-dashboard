d81b29080c1dce8030441dffbd4fd40a0aa2183f
Heraldo Valenti <heraldovalenti@gmail.com>
2020-11-27 03:35:40 -0300

    Added date conversion to UTC functions

--- src/utils/date.js
+++ src/utils/date.js
@@ -12,6 +12,11 @@ const plusHours = (now, hours) => now + hours * 1000 * 60 * 60;
 const plusDays = (now, days) => plusHours(now, days * 24);
 const getAesTimeString = (date) => moment(date).format('DD/MM/YYYY HH:mm');
 const getAesDateString = (date) => moment(date).format('DD/MM/YYYY');
+const localToUTC = (date) => {
+  const input = moment(date);
+  const utcOffset = input.utcOffset();
+  return input.add(utcOffset * -1, 'minutes');
+};
 export {
   getISODateString,
   getAmericanDateString,
@@ -22,4 +27,5 @@ export {
   plusDays,
   getAesTimeString,
   getAesDateString,
+  localToUTC,
 };
--- src/utils/test/date.test.js
+++ src/utils/test/date.test.js
@@ -3,6 +3,7 @@ import {
   getAmericanDateString,
   getDaysApart,
   getWeeksApart,
+  localToUTC,
 } from './../date';
 
 import moment from 'moment';
@@ -11,7 +12,8 @@ describe('Date', () => {
   describe('getISODateString', () => {
     it('initial time ISO date should be equal to 1970-01-01', () => {
       const initialLocalTimeDate = new Date(0);
-      const localDiffMilis = initialLocalTimeDate.getTimezoneOffset() * 60 * 1000;
+      const localDiffMilis =
+        initialLocalTimeDate.getTimezoneOffset() * 60 * 1000;
       const initialTimeDate = new Date(0 + localDiffMilis);
       expect(getISODateString(initialTimeDate)).toEqual('1970-01-01T00:00:00');
     });
@@ -19,7 +21,8 @@ describe('Date', () => {
   describe('getAmericanDateString', () => {
     it('initial time American date should be equal to Jan 01 1970', () => {
       const initialLocalTimeDate = new Date(0);
-      const localDiffMilis = initialLocalTimeDate.getTimezoneOffset() * 60 * 1000;
+      const localDiffMilis =
+        initialLocalTimeDate.getTimezoneOffset() * 60 * 1000;
       const initialTimeDate = new Date(0 + localDiffMilis);
       expect(getAmericanDateString(initialTimeDate)).toEqual(
         'Jan 01 1970 00:00'
@@ -41,5 +44,13 @@ describe('Date', () => {
       const bDate = moment('02/01/2020');
       expect(getWeeksApart(aDate, bDate)).toEqual(4);
     });
+  });
+});
+
+describe('localToUTC verification', () => {
+  it('should be 3 hours', () => {
+    const now = moment();
+    console.log(getISODateString(now));
+    console.log(getISODateString(localToUTC(now)));
   });
 });
