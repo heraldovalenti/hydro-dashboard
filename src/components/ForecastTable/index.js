a3a9d017c74f1315bf01b47435029ab65b69458e
Heraldo Valenti <heraldovalenti@gmail.com>
2023-11-07 09:42:15 -0300

    Fixed load loop on Forecast page

--- src/components/ForecastTable/index.js
+++ src/components/ForecastTable/index.js
@@ -13,24 +13,41 @@ import { fetchForecast } from '../../services/backend';
 import { useAppData } from '../../providers/AppDataProvider';
 import { getAesTimeString } from '../../utils/date';
 
-const ForecastInfo = ({ onClose }) => {
-  const { loading: appLoading } = useAppData();
-  const [loading, setLoading] = useState(false);
+const useForecast = () => {
+  const [loading, setLoading] = useState(true);
   const [forecast, setForecast] = useState({ forecasts: [], time: null });
-  const loadForecast = useCallback(
+
+  const loadForecast = useCallback(async (refresh = false) => {
+    const responseForecast = await fetchForecast(refresh);
+    setForecast(responseForecast);
+    setLoading(false);
+  }, []);
+
+  const reloadForecast = useCallback(
     async (refresh = false) => {
       if (!loading) {
         setLoading(true);
-        const responseForecast = await fetchForecast(refresh);
-        setForecast(responseForecast);
-        setLoading(false);
+        loadForecast(refresh);
       }
     },
-    [loading]
+    [loadForecast, loading]
   );
+
   useEffect(() => {
-    if (!appLoading) loadForecast();
-  }, [appLoading, loadForecast]);
+    loadForecast();
+  }, [loadForecast]);
+
+  return {
+    loading,
+    forecast,
+    reloadForecast,
+  };
+};
+
+const ForecastInfo = ({ onClose }) => {
+  const { loading: appLoading } = useAppData();
+  const { loading, forecast, reloadForecast } = useForecast();
+
   const { t } = useTranslation();
   return (
     <Container
@@ -45,7 +62,7 @@ const ForecastInfo = ({ onClose }) => {
           {t('forecast_page_title')}
         </Typography>
         <Box id="actions">
-          <IconButton onClick={() => loadForecast(true)}>
+          <IconButton onClick={() => reloadForecast(true)}>
             <Refresh />
           </IconButton>
           <IconButton onClick={() => onClose()}>
