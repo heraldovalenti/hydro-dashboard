ea993129d638e15667268257b719ee38f55b3ce7
Heraldo Valenti <heraldovalenti@gmail.com>
2023-11-08 13:11:35 -0300

    Removed unused APIs

--- src/services/HydroMetricStations/test/stations.test.js
+++
@@ -1,8 +0,0 @@
-import { HydroMetricStationRepository } from '../index';
-
-describe('read hydro metric stations json file', () => {
-  it('list stations', async () => {
-    const data = await HydroMetricStationRepository.list();
-    expect(data.length).toBe(10);
-  });
-});
--- src/services/WeatherStations/test/stations.test.js
+++
@@ -1,8 +0,0 @@
-import { WeatherStationRepository } from '../index';
-
-describe('read weather stations json file', () => {
-  it('list stations', async () => {
-    const data = await WeatherStationRepository.list();
-    expect(data.length).toBe(27);
-  });
-});
