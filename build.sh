a0ea6bb5b9b9cb092d007ca8c7174cf490dcf742
Heraldo Valenti <heraldovalenti@gmail.com>
2024-11-14 13:46:48 -0800

    Updated build script with mac options

--- build.sh
+++ build.sh
@@ -3,7 +3,10 @@
 IMAGE_NAME=aes-web
 DOCKER_BUILDER=node:16-alpine
 DOCKER_TAG=us-central1-docker.pkg.dev/hydro-dashboard-283320/aes-docker-repo/$IMAGE_NAME
+MAC_OPTIONS="--platform linux/arm64/v8"
+
 USE_DOCKER_BUILDER=0
+SKIP_PUSH=0
 
 VERSION=$1
 if [[ -z $VERSION ]]
@@ -22,11 +25,13 @@ fi
 for ARG in $@
 do
   if [[ $ARG == "--useDockerBuilder" ]]; then USE_DOCKER_BUILDER=1; fi
+  if [[ $ARG == "--skipPush" ]]; then SKIP_PUSH=1; fi
 done
 
 if [[ $USE_DOCKER_BUILDER == 1 ]]; then
   echo "executing app build..."
   docker run --rm \
+      $MAC_OPTIONS \
       -w /aes \
       -v $HOME/.npm:/root/.npm \
       -v $HOME/.yarn:/root/.yarn \
@@ -34,6 +39,7 @@ if [[ $USE_DOCKER_BUILDER == 1 ]]; then
       $DOCKER_BUILDER yarn
 
   docker run --rm \
+      $MAC_OPTIONS \
       -w /aes \
       -v $HOME/.npm:/root/.npm \
       -v $HOME/.yarn:/root/.yarn \
@@ -41,6 +47,7 @@ if [[ $USE_DOCKER_BUILDER == 1 ]]; then
       $DOCKER_BUILDER yarn clean
 
   docker run --rm \
+      $MAC_OPTIONS \
       -w /aes \
       -v $HOME/.npm:/root/.npm \
       -v $HOME/.yarn:/root/.yarn \
@@ -54,6 +61,8 @@ if [ ! -d build ]; then
   echo "Build directory is missing, cannot continue"
   exit 1
 fi
+
+if [[ $SKIP_PUSH == 1 ]]; then exit 0; fi
 
 docker build -t $IMAGE_NAME:$VERSION .
 
